5468065bfb173b4a8c3901d0aebc6033cbe9615d
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-01-29 12:02:37 -0300

    Manipulating folders with the File class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,25 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String[] lines = new String[] { "Good Morning", "Good Afternoon", "Good night" };
-		String path = "C:\\Workspace\\out.txt";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
 
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		File path = new File(strPath);
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("Files: ");
+		for (File file : files) {
+			System.out.println(file);
 		}
+		boolean sucess = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created sucessfully: " + sucess);
+		sc.close();
+
 	}
 }
